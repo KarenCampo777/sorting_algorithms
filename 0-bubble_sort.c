069360cbaac6ed8c8d66de1c5979751b7e71e6d4
KarenCampo777 <1472@holbertonschool.com>
2020-06-16 04:24:26 +0000

    adding task 0 with corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
+				bubble = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = bubble;
 				print_array(array, size);
